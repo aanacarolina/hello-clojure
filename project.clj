ca439e82211093e1157fa92a363442f3365ba225
carolina.silva <aanacarolina@gmail.com>
2024-03-27 10:34:19 -0300

    Add com.stuartsierra/component dependency

--- project.clj
+++ project.clj
@@ -10,7 +10,8 @@
                  [ch.qos.logback/logback-classic "1.2.10" :exclusions [org.slf4j/slf4j-api]]
                  [org.slf4j/jul-to-slf4j "1.7.35"]
                  [org.slf4j/jcl-over-slf4j "1.7.35"]
-                 [org.slf4j/log4j-over-slf4j "1.7.35"]]
+                 [org.slf4j/log4j-over-slf4j "1.7.35"]
+                 [com.stuartsierra/component "1.1.0"]]
   :min-lein-version "2.0.0"
   :resource-paths ["config", "resources"] 
   :profiles {:dev {:aliases {"run-dev" ["trampoline" "run" "-m" "hello-clojure.server/run-dev"]}
