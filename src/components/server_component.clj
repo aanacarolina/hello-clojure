fce01085295b957fc55724b4c3051f861d1d2d4f
carolina.silva <aanacarolina@gmail.com>
2024-03-28 16:56:26 -0300

    Refactor components and routes

--- src/components/server_component.clj
+++ src/components/server_component.clj
@@ -1,6 +1,8 @@
 (ns components.server-component
   (:require [com.stuartsierra.component :as component]
-            [io.pedestal.http :as http]))
+            [io.pedestal.http :as http]
+            [io.pedestal.http.body-params :as body-params]
+            [io.pedestal.http.route :as route]))
 
 
 (defrecord Server []
@@ -10,13 +12,31 @@
     (println ";; Starting Server")
     ;; In the 'start' method, a component may assume that its
     ;; dependencies are available and have already been started.
-    (assoc this :admin (get-user database "admin")))
+    (assoc this :server nil))
 
   (stop [this]
-    (println ";; Stopping ExampleComponent")
+    (println ";; Stopping Server")
     ;; Likewise, in the 'stop' method, a component may assume that its
     ;; dependencies will not be stopped until AFTER it is stopped.
     this))
+
+(def common-interceptors [(body-params/body-params)])
+;duvida: criar um outro def? entity-routes e add to ::http/routes
+
+(def routes
+  (route/expand-routes
+   #{["/hello" :get hello/respond-hello :route-name :hello]
+     ["/users" :get hello/all-users :route-name :users]
+     ["/users" :post (conj common-interceptors hello/create-user!) :route-name :create-user]
+     ["/users/:id" :get hello/user-by-id :route-name :user-by-id]
+     ["/users/q" :get hello/query-user :route-name :query-user-by-id]
+     ["/users/:id" :put (conj common-interceptors hello/update-user!) :route-name :update-user]
+     ["/users/:id" :delete hello/delete-user! :route-name :delete-user]
+     ;accounts starts here
+     ["/accounts/:user-id" :post (conj common-interceptors hello/create-account!) :route-name :create-account]
+     ["/user/account/:user-id" :get hello/user-accounts :route-name :user-accounts]
+     ;need to make deposit to have a history - to display the amount just change route above with :amount instead of :type 
+     #_["/users/:user-id/account/:account-id/type" :get user-deposits-by-account :route-name :user-deposits-by-account]}))
 
 
 (defn create-server []
@@ -25,8 +45,6 @@
     ::http/type :jetty
     ::http/port 7171
     ::http/join? false}))
-
-(defonce server (atom nil))
 
 (defn start []
   (try
--- src/core/components.clj
+++ src/core/components.clj
@@ -1,5 +1,15 @@
-(ns core.components)
+(ns core.components
+  (:require [com.stuartsierra.component :as component]))
 
 ;vai agir como main por enquanto no meu projeto
 ;system map e start aqui
 
+(defn hello-system-map []
+  (component/system-map 
+   :database (database/new-database)
+   :routes (routes/new-routes)
+   :webapp (component/using (webapp/new-webapp) [:config :database :routes])
+   :pedestal (component/using (component.pedestal/new-pedestal) [:config :database :routes :webapp])))
+
+
+(defn main [] (component/start (hello-system-map)))
