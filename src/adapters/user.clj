77c0c03eddbe5dfed83116fd7d5ea194d1ae645c
carolina.silva <aanacarolina@gmail.com>
2024-05-26 19:23:51 -0300

    hello wire-in

--- src/adapters/user.clj
+++ src/adapters/user.clj
@@ -2,8 +2,7 @@
   (:require [wire.in.user :as w.in.user]
             [wire.out.user-response :as w.out.user]
             [schema.core :as s]
-            [models.user]
-            ))
+            [models.user]))
 
 (s/defn wire-in->internal :- models.user/User
   [{:keys [name surname age]} :- w.in.user/UserRequest]
@@ -12,10 +11,8 @@
    :user/age age})
 
 (s/defn wire-in-hello->internal :- models.user/User
-  [{:keys [name surname age]} :- w.in.user/UserRequest]
-  {:user/name name
-   :user/surname surname
-   :user/age age})
+  [{:keys [name]} :- w.in.user/UserRequest]
+  {:name name})
 
 (s/defn internal->wire-out :- w.out.user/UserResponse
   [{:user/keys [id name surname age]} :- models.user/User]
