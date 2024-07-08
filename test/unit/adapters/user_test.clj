0e17611170412d6eb01668e86c27e78629339541
carolina.silva <aanacarolina@gmail.com>
2024-07-08 14:50:15 -0300

    add use-fix schema validation to tests

--- test/unit/adapters/user_test.clj
+++ test/unit/adapters/user_test.clj
@@ -1,22 +1,26 @@
-(ns unit.adapters.user-test(:require
-                            [clojure.test :refer :all]
-                            [adapters.user]
-                            [schema.core :as s] 
-                            [models.user]
-                            [wire.in.user]))
+(ns unit.adapters.user-test (:require
+                             [clojure.test :refer :all]
+                             [adapters.user]
+                             [schema.core :as s]
+                             [models.user]
+                             [wire.in.user]))
 
-;(s/set-fn-validation! true)
 
-(defonce user-info {:name "Fulana" :surname "De Silva" :age "42"})
+
+(defn my-test-fixture [f]
+  (s/set-fn-validation! true)
+  (f)
+  (s/set-fn-validation! false))
+
+
+(use-fixtures :once my-test-fixture)
+
+(def user-info {:name "Fulana" :surname "De Silva" :age 42})
 
 (deftest wire-in->internal-test
   (testing "adapts from request to internal"
-    (is (= {:user/name "Fulana" :user/surname "De Silva" :user/age "42"}
-           (adapters.user/wire-in->internal user-info))))
-  #_(testing "output type validation"
-    (is (nil? (s/check models.user/UserNew (adapters.user/wire-in->internal user-info)))))
-  #_(testing "input type validation"
-    (is (nil? (s/check wire.in.user/UserRequest user-info)))))
+    (is (= {:user/name "Fulana" :user/surname "De Silva" :user/age 42}
+           (adapters.user/wire-in->internal user-info)))))
 
 ;gen/fmap
 ;alura tests module
