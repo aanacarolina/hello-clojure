07d3fd385ac0df6e223330baa2d4b961681b17db
carolina.silva <aanacarolina@gmail.com>
2024-07-09 01:44:52 -0300

    fix arg

--- test/unit/user_test.clj
+++ test/unit/user_test.clj
@@ -2,8 +2,7 @@
   (:require
    [clojure.test :refer [deftest is testing]]
    [controllers.user]
-   [logic.user]
-   ))
+   [logic.user]))
 
 (defonce user-id (random-uuid)) 
 (defonce user-info {:name "Fulano" :surname "De Tal" :age 42}) 
@@ -13,10 +12,9 @@
     (is (= {:name "Fulano" :surname "De Tal" :age 42 :user/id user-id}
            (logic.user/new-user user-id user-info)))))
 
-
 (deftest wire-in->internal-test 
   (testing "user from req to internal"
-    (is (= {:user/name "Fulano" :user/surname "De Tal" :user/age 42} ()))))
+    (is (= {:user/name "Fulano" :user/surname "De Tal" :user/age 42} user-info))))
 
 
 
