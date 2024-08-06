be45f285be98eee9ee2490b65b3d4f4d8b7b2148
carolina.silva <aanacarolina@gmail.com>
2024-08-06 18:54:47 -0300

    STATE PROBLEMS

--- test/integration/user.clj
+++ test/integration/user.clj
@@ -50,26 +50,140 @@
 
 ;-----
 
-(def user-created {:id  (uuid?)
-                   :name "John"
-                   :surname "Doe"
-                   :age 30})
 
 (defflow hello-endpoint
   (flow "Given a request without a name (query param) to /hello, should return hello world  message"
         (match? {:status 200, :body "Hello, stranger \n"}  (helper.http/request :get "/hello"))))
 
+; NIL
+(defflow create-user-endpoint-1
+  (flow "Given a request to create a user should return a user"
+        [completed-user-creation (helper.http/request :post "/users"
+                                                      "{ \"name\": \"John\",
+                                                                 \"surname\": \"Doe\",
+                                                                 \"age\": 30}")]
+   
+               (match? {:status 201, :body {:id (:id (:body completed-user-creation))   
+                                            :name "John"
+                                            :surname "Doe"
+                                            :age 30}} completed-user-creation)
+
+        #_(flow "Given a user creation should be able to query this user on DB to confirm persistance" ;flow dentro do mesmo flow! 
+                (match?  {:id id
+                          :name "John"
+                          :surname "Doe"
+                          :age 30}
+                         (helper.db/datomic-query (:id  (:id completed-user-creation)))))))
+
+ ; FAIL in integration.user/create-user-endpoint:
+; ; create-user-endpoint -> Given a request to create a user should return a user (user.clj:60) -> match? (user.clj:66) -> (cats.core/do-let ...)
+; ; expected:
+; {:status 201, :body {:id nil, :name "John", :surname "Doe", :age 30}}
+; 
+; ; actual:
+; {:status 201,
+;  :body
+;  (mismatch
+;   (expected {:id nil, :name John, :surname Doe, :age 30})
+;   (actual
+;    {:id #uuid "cba6458e-487c-4cd6-8959-d309c9d0ca38", :name "John", :surname "Doe", :age 30})),
+;  :headers
+;  {Sxxxxx}}
+
+
+;nil
 (defflow create-user-endpoint
   (flow "Given a request to create a user should return a user"
-        (match? {:status 201, #_#_:body user-created} ;WITH REDEF DO ID? OU ALGO DO STATE-FLOW
-                (helper.http/request :post "/users"
-                                      "{ \"name\": \"John\",
-                                        \"surname\": \"Doe\",
-                                        \"age\": 30}"))
-        (flow "Given a user creation should be able to query this user on DB to confirm persistance" ;flow dentro do mesmo flow! 
-                (match? user-created
-                        (helper.db/datomic-query (:id user-created))))))
+        [completed-user-creation (helper.http/request :post "/users"
+                                                      "{ \"name\": \"John\",
+                                                                 \"surname\": \"Doe\",
+                                                                 \"age\": 30}")
+         id (:id (:body completed-user-creation))]
+        (match? {:status 201, :body {:id id
+                                     :name "John"
+                                     :surname "Doe"
+                                     :age 30}} completed-user-creation)
 
+        #_(flow "Given a user creation should be able to query this user on DB to confirm persistance" ;flow dentro do mesmo flow! 
+                (match?  {:id id
+                          :name "John"
+                          :surname "Doe"
+                          :age 30}
+                         (helper.db/datomic-query (:id  (:id completed-user-creation)))))))
+
+
+;exception
+(defflow create-user-endpoint-2
+  (flow "Given a request to create a user should return a user"
+        [completed-user-creation (helper.http/request :post "/users"
+                                                      "{ \"name\": \"John\",
+                                                                 \"surname\": \"Doe\",
+                                                                 \"age\": 30}")
+         id (get-in completed-user-creation [:body :id])]
+        (match? {:status 201, :body {:id id
+                                     :name "John"
+                                     :surname "Doe"
+                                     :age 30}} completed-user-creation)
+
+        #_(flow "Given a user creation should be able to query this user on DB to confirm persistance" ;flow dentro do mesmo flow! 
+                (match?  {:id id
+                          :name "John"
+                          :surname "Doe"
+                          :age 30}
+                         (helper.db/datomic-query (:id  (:id completed-user-creation)))))))
+
+;                        state-flow.core/clarify-illegal-arg       core.clj:  137
+; clojure.lang.ExceptionInfo: Expected a flow, got class: nil
+; 
+; ERROR in integration.user/create-user-endpoint-2 (core.clj):
+; Uncaught exception, not in assertion
+; error: clojure.lang.ExceptionInfo: Flow "create-user-endpoint-2 -> Given a request to create a user should return a user (user.clj:118) -> [completed-user-creation ...]" failed with exception {} (core.clj)
+
+
+;alem do binding tbm parece que o problema eh coom uuid, pq com body fica ok
+(defflow create-user-endpoint-3
+  (flow "Given a request to create a user should return a user"
+        [completed-user-creation (helper.http/request :post "/users"
+                                                      "{ \"name\": \"John\",
+                                                                 \"surname\": \"Doe\",
+                                                                 \"age\": 30}")]
+        (match? {:status 201, :body {:id (:body completed-user-creation)
+                                     :name "John"
+                                     :surname "Doe"
+                                     :age 30}} completed-user-creation)
+
+        #_(flow "Given a user creation should be able to query this user on DB to confirm persistance" ;flow dentro do mesmo flow! 
+                (match?  {:id id
+                          :name "John"
+                          :surname "Doe"
+                          :age 30}
+                         (helper.db/datomic-query (:id  (:id completed-user-creation)))))))
+
+; ; FAIL in integration.user/create-user-endpoint-3:
+; ; create-user-endpoint-3 -> Given a request to create a user should return a user (user.clj:145) -> match? (user.clj:151) -> (cats.core/do-let ...)
+; ; expected:
+; {:status 201,
+;  :body
+;  {:id
+;   "{:id #uuid \"3c487dcc-669c-4453-b16f-2508595a9ed7\", :name \"John\", :surname \"Doe\", :age 30}",
+;   :name "John",
+;   :surname "Doe",
+;   :age 30}}
+; 
+; ; actual:
+; {:status 201,
+;  :body
+;  (mismatch
+;   (expected
+;    {:id
+;          {:id #uuid "3c487dcc-669c-4453-b16f-2508595a9ed7", :name "John", :surname "Doe", :age 30},
+;          :name John,
+;          :surname Doe,
+;          :age 30})
+;   (actual
+;    {:id #uuid "3c487dcc-669c-4453-b16f-2508595a9ed7", :name "John", :surname "Doe", :age 30})),
+;  :headers
+;  {...;
 ;TODO test create endpoints
 
 
