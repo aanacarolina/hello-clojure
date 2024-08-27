feed9563a508641eddbebdc57839b7c11c870af3
carolina.silva <aanacarolina@gmail.com>
2024-08-27 15:12:21 -0300

    wip - integration tests

--- test/integration/aux/http.clj
+++ test/integration/aux/http.clj
@@ -1,35 +1,20 @@
 (ns integration.aux.http
-   (:require
+  (:require
    [state-flow.api :refer [flow get-state return]]
    [io.pedestal.http :as http]
    [io.pedestal.test :as test]
-     [clojure.edn :as edn]))
+   [clojure.edn :as edn]))
 
 
 (def fetch-servelet
   (comp ::http/service-fn deref :server :server :system))
 
-
-
-(defn *request
-  ([http verb url]
-   (let [{:keys [status body]} (test/response-for http verb url)]
-     {:status status
-      :body   (edn/read-string body)}))
-  ([http verb url body-request]
-   (let [{:keys [status body]} (test/response-for http verb url
-                                                  :body body-request
-                                                  :headers {"Content-Type" "application/json"})]
-     {:status status
-      :body   (edn/read-string body)})))
-
 (defn request
   ([verb url]
    (flow "make request"
          [http (get-state fetch-servelet)]
-         (return (*request http verb url))))
+         (return (test/response-for http verb url))))
   ([verb url body]
    (flow "make request"
-         [http (get-state fetch-servelet)] 
-         (return (*request http verb url body)))))
-
+         [http (get-state fetch-servelet)]
+         (return (test/response-for http verb url :body (edn/read-string body))))))
--- test/integration/user.clj
+++ test/integration/user.clj
@@ -8,30 +8,32 @@
 
 
 (def user-json "{ \"name\": \"John\", \"surname\": \"Doe\", \"age\": 30}")
+(def user-uuid "3ae1dc15-b3ff-4529-be66-38c6ececc7bb")
+
+
 
 (defflow hello-endpoint
   (flow "Given a request without a name (query param) to /hello, should return hello world  message"
-        (match? {:status 200, :body {:msg "Hello, stranger"}}  (helper.http/request :get "/hello"))))
+        (match? {:status 200, :body {:msg "Hello, stranger"} :headers {"Content-Type" "application/edn"}}
+                (helper.http/request :get "/hello"))))
+
+
 
 (defflow create-user-endpoint
   (flow "Given a request to create a user should return a user"
-        [completed-user-creation (helper.http/request :post "/users" user-json)
-         created-id (return (:id (:body completed-user-creation)))]
-        (match? {:status 201, :body {:id created-id
-                                     :name "John"
-                                     :surname "Doe"
-                                     :age 30}} completed-user-creation)
+                (match? {:status 201, :body {:name "John", :surname "Doe", :age 30} :headers {"Content-Type" "application/edn"}}
+                (helper.http/request :post "/users" user-json ))
 
-        (flow "Given a succesful user creation should query this user on DB to confirm persistance" ;flow dentro do mesmo flow! 
-              (match?  {:id id
+       #_ (flow "Given a succesful user creation should query this user on DB to confirm persistance" ;flow dentro do mesmo flow! 
+              (match?  {:id created-id
                         :name "John"
                         :surname "Doe"
                         :age 30}
                        (helper.db/datomic-query (:id  (:id completed-user-creation)))))))
 
-(defflow get-user-by-id-endpoint
-  (flow "Given a request to create a user should return a user"
-        [completed-user-creation (helper.http/request :post "/users" user-json)
+#_(defflow get-user-by-id-endpoint
+  (flow "Given an existent user query by its id (uuid) and should return a user"
+        [completed-user-creation (helper.http/request :get "/users/:id" user-uuid)
          created-id (return (:id (:body completed-user-creation)))]
         (match? {:status 201, :body {:id created-id
                                      :name "John"
