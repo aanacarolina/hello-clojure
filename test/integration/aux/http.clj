1efa9cbf114c2923fd637505752fde7f89992d25
carolina.silva <aanacarolina@gmail.com>
2024-09-02 08:25:03 -0300

    nada funciona

--- test/integration/aux/http.clj
+++ test/integration/aux/http.clj
@@ -3,18 +3,40 @@
    [state-flow.api :refer [flow get-state return]]
    [io.pedestal.http :as http]
    [io.pedestal.test :as test]
-   [clojure.edn :as edn]))
+   [clojure.edn :as edn]
+   #_[data.json :as json]))
 
 
 (def fetch-servelet
   (comp ::http/service-fn deref :server :server :system))
+
+
+ ;& args variadic - qdo body nao existir nao envia na req / a resposta eh igual {:status and :body} . se body read-string
+(defn *request
+  ([verb url]
+   (let  [http  (get-state fetch-servelet)
+          {:keys [status body]} (test/response-for http verb url)]
+     {:status status :body body :headers {"Content-Type" "application/edn"}}))
+  ([verb url body-request]
+   (let  [http  (get-state fetch-servelet)
+          {:keys [status body]} (test/response-for http verb url body-request)]
+     {:status status :body body :headers {"Content-Type" "application/edn"}})))
 
 (defn request
   ([verb url]
    (flow "make request"
          [http (get-state fetch-servelet)]
          (return (test/response-for http verb url))))
-  ([verb url body]
+  ([verb url body] ;& args variadic - qdo body nao existir nao envia na req / a resposta eh igual {:status and body} . se body read-string
    (flow "make request"
          [http (get-state fetch-servelet)]
-         (return (test/response-for http verb url :body (edn/read-string body))))))
+         (return (test/response-for http verb url :body body)))))
+
+
+(defn request-v2
+  ([verb url]
+   (flow "make request" 
+         (return (*request verb url))))
+  ([verb url body-request] 
+   (flow "make request" 
+         (return (*request verb url body-request)))))
--- test/integration/user.clj
+++ test/integration/user.clj
@@ -4,25 +4,26 @@
    [state-flow.state :as state]
    [integration.aux.init :refer [defflow]]
    [integration.aux.http :as helper.http]
-   [integration.aux.db :as helper.db]))
+   [integration.aux.db :as helper.db] 
+     [clojure.edn :as edn]
+   ))
 
 
 (def user-json "{ \"name\": \"John\", \"surname\": \"Doe\", \"age\": 30}")
 (def user-uuid "3ae1dc15-b3ff-4529-be66-38c6ececc7bb")
 
-
-
 (defflow hello-endpoint
   (flow "Given a request without a name (query param) to /hello, should return hello world  message"
-        (match? {:status 200, :body {:msg "Hello, stranger"} :headers {"Content-Type" "application/edn"}}
-                (helper.http/request :get "/hello"))))
-
+        [say-hello (return (:body (helper.http/request :get "/hello"))) ]
+        (match? {:body {:msg "Hello, stranger"}}
+                say-hello)))
 
 
 (defflow create-user-endpoint
   (flow "Given a request to create a user should return a user"
-                (match? {:status 201, :body {:name "John", :surname "Doe", :age 30} :headers {"Content-Type" "application/edn"}}
-                (helper.http/request :post "/users" user-json ))
+        [completed-user-creation (helper.http/request :post "/users" user-json)]
+                (match? 1
+                completed-user-creation)
 
        #_ (flow "Given a succesful user creation should query this user on DB to confirm persistance" ;flow dentro do mesmo flow! 
               (match?  {:id created-id
